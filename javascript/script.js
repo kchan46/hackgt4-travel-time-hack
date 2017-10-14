3023e002c676502352ae1cd3f78db4770a9f2cd3
kchan46 <kchan46@gatech.edu>
2017-10-14 03:08:42 -0400

    finished barebones UI

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
-document.getElementById("next_page").onclick = function () {
+function redirect() {
     location.href="second_page.html";
+}
+
+function redirect2() {
+    location.href="results.html";
+}
+
+function redirect3() {
+    location.href="index.html";
 }

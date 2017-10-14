ee7fc271db8629b604caebe28b56271f992dc18a
kmorris72 <kipp.morris@gmail.com>
2017-10-14 16:20:39 -0400

    updated script.js to store data user inputs on first page for later access

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+var firstPageData=new Array();
+
 function redirect() {
+    firstPageData.push(document.getElementById("address").value);
+    firstPageData.push(document.getElementById("airport").value);
+    firstPageData.push(document.getElementById("flight_number").value);
     location.href="second_page.html";
 }
 
@@ -8,4 +13,16 @@ function redirect2() {
 
 function redirect3() {
     location.href="index.html";
+}
+
+function getAddress() {
+    return firstPageData[0];
+}
+
+function getAirport() {
+    return firstPageData[1];
+}
+
+function getFlightNumber() {
+    return firstPageData[2];
 }

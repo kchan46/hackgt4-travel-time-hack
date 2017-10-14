4679fbad60679b8b1cfdfe3b1a1174a34ebdab9c
kmorris72 <kipp.morris@gmail.com>
2017-10-14 17:35:21 -0400

    updated script.js to store data user inputs on first page for later access

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,17 @@
-var firstPageData = new Array();
+var myStorage = window.localStorage;
+
 function redirect() {
- firstPageData.push(document.getElementById("address").value);
+    myStorage.setItem("address", document.getElementById("address").value);
+    myStorage.setItem("airport", document.getElementById("airport").value);
+    myStorage.setItem("flight_number", document.getElementById("flight_number").value);
     location.href="second_page.html";
-    
-    firstPageData.push(document.getElementById("date_of_flight").value);
-    firstPageData.push(document.getElementById("flight_number").value);
 }
 
 function redirect2() {
+    myStorage.setItem("routine", document.getElementById("routine").value);
+    myStorage.setItem("transportation_mode", document.getElementById("transportation_mode").value);
+    myStorage.setItem("TSA-precheck", document.getElementById("TSA-precheck").value);
+    myStorage.setItem("phone", document.getElementById("phone").value);
     location.href="results.html";
 }
 
@@ -15,12 +19,30 @@ function redirect3() {
     location.href="index.html";
 }
 
-function returnOrigin() 
-{
-
-   return document.getElementById("address").value;
+function getAddress() {
+    return myStorage.getItem("address");
 }
 
-function getAddress() {
-    return firstPageData[0];
+function getAirport() {
+    return myStorage.getItem("airport");
+}
+
+function getFlightNumber() {
+    return myStorage.getItem("flight_number");
+}
+
+function getRoutine() {
+    return myStorage.getItem("routine");
+}
+
+function getTransportationMode() {
+    return myStorage.getItem("transportation_mode");
+}
+
+function getTSA() {
+    return myStorage.getItem("TSA-precheck");
+}
+
+function getPhone() {
+    return myStorage.getItem("phone");
 }
